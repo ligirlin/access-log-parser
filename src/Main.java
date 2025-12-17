0fddef17086e169f3fc96b56983714f4bebb04df
Olga Samorodova <ligirlin@gmail.com>
2025-12-17 12:33:32 +0300

    есконечный цикл while, в котором в консоли будет запрашиваться путь к файлу, который программа затем будет парсить

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int correctFilesCount = 0;
 
-        System.out.println("Введите текст и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String filePath = scanner.nextLine();
 
+            File file = new File(filePath);
+            boolean fileExists = file.exists();
+            boolean isFile = file.isFile();
+
+            if (!fileExists) {
+                System.out.println("Файл не существует");
+                continue;
+            }
+            if (isFile) {
+                System.out.println("Указан путь к папке, а не к файлу!");
+                continue;
+            }
+            correctFilesCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + correctFilesCount);
+        }
     }
 }
